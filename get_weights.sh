ed1de45b3a0c8554ecb2aef2c5daae933700203c
cyber-art <cuy.official@gmail.com>
2021-09-29 21:35:19 +0100

    Bash script to fetch YOLOv3 and YOLOv3-tiny weights

---
+++ get_weights.sh
@@ -0,0 +1,6 @@
+# get YOLOv3 weights
+wget https://pjreddie.com/media/files/yolov3.weights -O ./weights/yolov3.weights
+
+# get YOLOv3-tiny weights
+wget https://pjreddie.com/media/files/yolov3-tiny.weights -O ./weights/yolov3-tiny.weights
+
