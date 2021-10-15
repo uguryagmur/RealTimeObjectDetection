60894d1d66fa80339cd32e49d24ceaa2499f42a7
cyber-art <cuy.official@gmail.com>
2021-10-15 08:51:57 +0100

    refactoring training

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 """YOLOv3 Darknet Trainer of the Network"""
 
-import numpy as np
-from PIL import Image
-from PIL import ImageDraw
 import time
 import torch
 import argparse
@@ -14,14 +11,13 @@ from src.dataset import COCO, VOC
 from src.darknet import Darknet
 from src.util import xywh2YOLO, bbox_iou_wh
 
-torch.manual_seed(7)
+torch.manual_seed(0)
 
 
 class DarknetTrainer:
     """Darknet YOLO Network Trainer Class
 
     Attributes:
-        img_size (list, tuple): Size of the training images
         epoch (int): Epoch number of the training
         batch_size (int): Size of the mini-batches
         dataset (COCO): Dataset to train network
@@ -61,13 +57,13 @@ class DarknetTrainer:
         self.optimizer = optim.Adam(self.darknet.parameters(), lr=1e-2)
         self.validator = None
         self.history = dict()
-        if cfg_file[-8:-4] == 'tiny':
-            self.TINY = True
-        else:
-            self.TINY = False
-        if weights_file is not None:
-            self.darknet.load_weights(weights_file)
+        self.configure_for_tiny_model(cfg_file)
+        self.load_weights(weights_file)
+        self.configure_darknet(TUNE)
+        print("\nTrainer is ready!!\n")
+        print('GPU usage = {}\n'.format(self.CUDA))
 
+    def configure_darknet(self, TUNE):
         # using GPUs for training
         self.device = torch.device('cuda:0' if self.CUDA
                                    else 'cpu')
@@ -79,8 +75,16 @@ class DarknetTrainer:
             self.optimizer.load_state_dict(
                 torch.load('weights/training_output_opt'))
         self.darknet = self.darknet.train()
-        print("\nTrainer is ready!!\n")
-        print('GPU usage = {}\n'.format(self.CUDA))
+
+    def load_weights(self, weights_file):
+        if weights_file is not None:
+            self.darknet.load_weights(weights_file)
+
+    def configure_for_tiny_model(self, cfg_file):
+        if cfg_file[-8:-4] == 'tiny':
+            self.TINY = True
+        else:
+            self.TINY = False
 
     def COCO_loader(self, json_dir, img_dir,
                     batch_size, shuffle) -> None:
@@ -138,15 +142,27 @@ class DarknetTrainer:
             layer_1, mask_1 = self.target_layer(bndbox[i], 13, anchors[:3])
             layer_2, mask_2 = self.target_layer(bndbox[i], 26, anchors[3:6])
             if self.TINY:
-                output.append(torch.cat((layer_1, layer_2), dim=0))
-                mask.append(torch.cat((mask_1, mask_2), dim=0))
+                self.create_target_layer_for_tiny(layer_1, layer_2, mask, mask_1, mask_2, output)
             else:
-                layer_3, mask_3 = self.target_layer(bndbox[i], 52, anchors[6:])
-                output.append(torch.cat((layer_1, layer_2, layer_3), dim=0))
-                mask.append(torch.cat((mask_1, mask_2, mask_3), dim=0))
+                self.create_target_layer_for_main(anchors, bndbox, i, layer_1, layer_2, mask, mask_1, mask_2, output)
+        mask, output = self.stack_output_and_mask(mask, output)
+        return output, mask
+
+    @staticmethod
+    def stack_output_and_mask(mask, output):
         output = torch.stack(output, dim=0)
         mask = torch.stack(mask, dim=0).bool()
-        return output, mask
+        return mask, output
+
+    def create_target_layer_for_main(self, anchors, bndbox, i, layer_1, layer_2, mask, mask_1, mask_2, output):
+        layer_3, mask_3 = self.target_layer(bndbox[i], 52, anchors[6:])
+        output.append(torch.cat((layer_1, layer_2, layer_3), dim=0))
+        mask.append(torch.cat((mask_1, mask_2, mask_3), dim=0))
+
+    @staticmethod
+    def create_target_layer_for_tiny(layer_1, layer_2, mask, mask_1, mask_2, output):
+        output.append(torch.cat((layer_1, layer_2), dim=0))
+        mask.append(torch.cat((mask_1, mask_2), dim=0))
 
     def target_layer(self, bboxes: torch.Tensor, scale, anchors):
         """Function to create target for layer with respect to given scale
@@ -160,34 +176,24 @@ class DarknetTrainer:
         output = torch.zeros((scale*scale*len(anchors),
                               5 + self.num_classes))
         mask = torch.zeros(output.shape[:-1])
-        # zeros = torch.zeros(5 + self.num_classes)
         stride = self.resolution//scale
-        # for 26 scale in the below for loop THERE IS A STUPID FUCKING MISTAKE
         for box in bboxes:
             if box[5] != 1:
                 continue
             elif box[2] < 24 or box[3] < 24:
                 continue
-            # print('ANALYSIS')
-            # print(box[:6])
             anchor_fit = self.anchor_fit(box[:4], anchors)
             best_anchor = anchors[anchor_fit]
-            # print('ANALYSIS')
-            # print(stride)
-            # print(box[:5], scale)
             w_coor, h_coor, w_center, h_center, w, h = xywh2YOLO(box, stride,
                                                                  best_anchor)
-            # print(x, y, x_, y_, w_, h_, scale)
-            # print('BOX= ', best_anchor)
             loc = (w_coor*scale + h_coor)*len(anchors) + anchor_fit
-            # print(loc)
-            # print('----------------------------')
             output[loc] = box
             output[loc][:4] = torch.FloatTensor([w_center, h_center, w, h])
             mask[loc] = 1
         return output, mask
 
-    def anchor_fit(self, box: torch.Tensor, anchors: list):
+    @staticmethod
+    def anchor_fit(box: torch.Tensor, anchors: list):
         """Function to return best fitting anchor box to the corresponding
         bounding box
 
@@ -296,13 +302,7 @@ class DarknetTrainer:
             annotation_dir (str): Path of the annotation for the training
             img_dir (str): Directory of the folder containing dataset images
         """
-
-        assert isinstance(annotation_dir, str)
-        assert isinstance(img_dir, str)
-        self.history['train_loss'] = []
-        self.history['train_precision'] = []
-        self.history['train_recall'] = []
-        self.history['train_f_score'] = []
+        self.initialize_training()
         best_metric = None
 
         # dataloader adjustment
@@ -312,98 +312,124 @@ class DarknetTrainer:
 
         batch_num = self.data_num//self.batch_size + 1
         for epoch in range(1, self.epoch+1):
-            running_loss = 0.0
-            t_start = time.time()
-
-            # training mini-batches
-            for batch, batch_samples in enumerate(self.dataloader):
-                samples = batch_samples[0]
-                bndbox = batch_samples[1]
-
-                if self.CUDA:
-                    batch_data = samples.clone().cuda()
-                else:
-                    batch_data = samples.clone()
-                del batch_samples, samples
-
-                # making the optimizer gradient zero
-                self.optimizer.zero_grad()
-                with self.darknet.train_mode():
-                    pred = self.darknet(batch_data)
-
-                # creating the target of the training
-                target, mask = self.target_creator(bndbox)
-                if self.CUDA:
-                    target = target.cuda()
-
-                # calculating the loss and backpropagation
-                loss = self.criterion(pred, target, mask)
-                loss.backward()
-                self.optimizer.step()
-
-                # loss at the end of the batch
-                running_loss += loss.item()
-                self.progress_bar(epoch, self.epoch, batch+1,
-                                  batch_num, loss.item())
-                torch.cuda.empty_cache()
-
-            # saving the weights of each epoch
-            torch.save(self.darknet.state_dict(),
-                       'weights/weight_epoch' + str(epoch))
-
-            # saving checkpoint with respect to minimum loss if no validator
-            if self.validator is None:
-                if best_metric is None or running_loss > best_metric:
-                    best_metric = running_loss
-                    torch.save(self.darknet.state_dict(),
-                               'weights/checkpoint')
-                    torch.save(self.optimizer.state_dict(),
-                               'weights/checkpoint_opt')
-
-            # saving checkpoint with respect to validation
-            else:
-                self.validator.validate_model(self.darknet, CUDA=self.CUDA)
-                self.history['train_precision'].append(
-                    self.validator.precision)
-                self.history['train_recall'].append(self.validator.recall)
-                self.history['train_f_score'].append(self.validator.f_score)
-                if best_metric is None or self.validator.f_score > best_metric:
-                    best_metric = self.validator.f_score
-                    torch.save(self.darknet.state_dict(),
-                               'weights/checkpoint')
-                    torch.save(self.optimizer.state_dict(),
-                               'weights/checkpoint_opt')
-
-            # printing the metrics of the epoch
-            t_end = time.time()
-            self.epoch_loss(running_loss, self.dataset.__len__())
-            self.epoch_ETA(t_start, t_end, remaining_epoch=(self.epoch-epoch))
-            self.history['train_loss'].append(running_loss/(batch_num))
+            running_loss, t_start = self.forward_epoch(batch_num, best_metric, epoch)
+            self.update_epoch_info(batch_num, epoch, running_loss, t_start)
 
         # when the training is finished
-        torch.save(self.darknet.state_dict(),
-                   'weights/training_output')
-        torch.save(self.optimizer.state_dict(),
-                   'weights/training_output_opt')
+        self.save_end_checkpoint_and_optimizer()
         epochs = [item for item in range(1, len(self.history['train_loss'])+1)]
 
-        # optaining the loss graph
+        self.plot_loss_graph(epochs)
+        if self.validator is not None:
+            self.plot_validation_graph(epochs)
+        print('Training is finished !!\n')
+
+    def initialize_training(self):
+        self.history['train_loss'] = []
+        self.history['train_precision'] = []
+        self.history['train_recall'] = []
+        self.history['train_f_score'] = []
+
+    def forward_epoch(self, batch_num, best_metric, epoch):
+        running_loss = 0.0
+        t_start = time.time()
+        # training mini-batches
+        for batch, batch_samples in enumerate(self.dataloader):
+            running_loss = self.forward_batch(batch, batch_num, batch_samples, epoch, running_loss)
+
+        torch.save(self.darknet.state_dict(),
+                   'weights/weight_epoch' + str(epoch))
+
+        if self.validator is None:
+            best_metric = self.save_checkpoint(best_metric, running_loss)
+        else:
+            self.update_scores()
+            best_metric = self.save_checkpoint(best_metric, self.validator.f_score)
+        return running_loss, t_start
+
+    def forward_batch(self, batch, batch_num, batch_samples, epoch, running_loss):
+        samples = batch_samples[0]
+        bndbox = batch_samples[1]
+        batch_data = self.move_data_to_device(samples)
+        del batch_samples, samples
+        loss = self.feed_forward_through_model(batch_data, bndbox)
+        # loss at the end of the batch
+        running_loss = self.update_batch_info(batch, batch_num, epoch, loss, running_loss)
+        return running_loss
+
+    def plot_validation_graph(self, epochs):
+        plt.plot(epochs, self.history['train_precision'], color='blue')
+        plt.plot(epochs, self.history['train_recall'], color='green')
+        plt.plot(epochs, self.history['train_f_score'], color='yellow')
+        plt.legend(['precision', 'recall', 'f_score'])
+        plt.xlabel('epoch number')
+        plt.ylabel('metrics')
+        plt.savefig('weights/metric_graph.png')
+
+    def plot_loss_graph(self, epochs):
         plt.plot(epochs, self.history['train_loss'], color='red')
         plt.xlabel('epoch number')
         plt.ylabel('loss')
         plt.savefig('weights/loss_graph.png')
         plt.clf()
 
-        # obtaining the validation graph
-        if self.validator is not None:
-            plt.plot(epochs, self.history['train_precision'], color='blue')
-            plt.plot(epochs, self.history['train_recall'], color='green')
-            plt.plot(epochs, self.history['train_f_score'], color='yellow')
-            plt.legend(['precision', 'recall', 'f_score'])
-            plt.xlabel('epoch number')
-            plt.ylabel('metrics')
-            plt.savefig('weights/metric_graph.png')
-        print('Training is finished !!\n')
+    def save_end_checkpoint_and_optimizer(self):
+        torch.save(self.darknet.state_dict(),
+                   'weights/training_output')
+        torch.save(self.optimizer.state_dict(),
+                   'weights/training_output_opt')
+
+    def update_epoch_info(self, batch_num, epoch, running_loss, t_start):
+        # printing the metrics of the epoch
+        t_end = time.time()
+        self.epoch_loss(running_loss, self.dataset.__len__())
+        self.epoch_ETA(t_start, t_end, remaining_epoch=(self.epoch - epoch))
+        self.history['train_loss'].append(running_loss / (batch_num))
+
+    def update_batch_info(self, batch, batch_num, epoch, loss, running_loss):
+        running_loss += loss.item()
+        self.progress_bar(epoch, self.epoch, batch + 1,
+                          batch_num, loss.item())
+        torch.cuda.empty_cache()
+        return running_loss
+
+    def update_scores(self):
+        self.validator.validate_model(self.darknet, CUDA=self.CUDA)
+        self.history['train_precision'].append(
+            self.validator.precision)
+        self.history['train_recall'].append(self.validator.recall)
+        self.history['train_f_score'].append(self.validator.f_score)
+
+    def save_checkpoint(self, best_metric, running_loss):
+        if best_metric is None or running_loss > best_metric:
+            best_metric = running_loss
+            torch.save(self.darknet.state_dict(),
+                       'weights/checkpoint')
+            torch.save(self.optimizer.state_dict(),
+                       'weights/checkpoint_opt')
+        return best_metric
+
+    def feed_forward_through_model(self, batch_data, bndbox):
+        # making the optimizer gradient zero
+        self.optimizer.zero_grad()
+        with self.darknet.train_mode():
+            pred = self.darknet(batch_data)
+        # creating the target of the training
+        target, mask = self.target_creator(bndbox)
+        if self.CUDA:
+            target = target.cuda()
+        # calculating the loss and backpropagation
+        loss = self.criterion(pred, target, mask)
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def move_data_to_device(self, samples):
+        if self.CUDA:
+            batch_data = samples.clone().cuda()
+        else:
+            batch_data = samples.clone()
+        return batch_data
 
 
 def arg_parse():
